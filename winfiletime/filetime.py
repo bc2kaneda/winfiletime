a2552fcc81791e08c938004b1e0a100dcc7ca206
Martin Johansson <martin@bc2.se>
2024-07-26 11:45:20 +0200

    Replace deprecated function utcfromtimestamp

--- winfiletime/filetime.py
+++ winfiletime/filetime.py
@@ -27,4 +27,4 @@ def to_datetime(filetime: int) -> datetime:
 	# Get seconds and remainder in terms of Unix epoch
 	s, ns100 = divmod(filetime - EPOCH_AS_FILETIME, HUNDREDS_OF_NS)
 	# Convert to datetime object, with remainder as microseconds.
-	return datetime.utcfromtimestamp(s).replace(microsecond=(ns100 // 10))
+	return datetime.fromtimestamp(s, timezone.utc).replace(microsecond=(ns100 // 10))
